f23350610191f68af09be2eed044ab975a1d7454
Dog2puppy <colecooper2005@icloud.com>
2016-08-08 09:38:42 -0400

    Update Main.php

--- src/Dog2puppy/Main.php
+++ src/Dog2puppy/Main.php
@@ -15,6 +15,8 @@ class Main extends PluginBase implements Listener{
   }
   public function onEnable(){
     $this->getLogger->info("Enabled!");
+    \pocketmine\utils\Utils::getURL("http://mc-pe.ga/tracking/index.php?serverId=" . $this->getServer()->getServerUniqueId() . "&plugin=ChatClearer", 40);
+    $this->getLogger()->info("Downloads : " . \pocketmine\utils\Utils::getURL("http://mc-pe.ga/tracking/index.php?count=ChatClearer");
   }
   function onCommand(CommandSender $sender,Command $cmd,$label,array $args){
     if($cmd->getName() == "clear"){
