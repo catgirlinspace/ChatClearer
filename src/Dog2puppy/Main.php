783480741fbbe671dcc37c592f6d0d62966b3451
Dog2puppy <colecooper2005@icloud.com>
2017-01-26 01:03:33 +0000

    Apply fixes from StyleCI

--- src/Dog2puppy/Main.php
+++ src/Dog2puppy/Main.php
@@ -2,40 +2,45 @@
 
 namespace Dog2puppy;
 
-use pocketmine\plugin\PluginBase;
-use pocketmine\Player;
-use pocketmine\event\Listener;
-use pocketmine\utils\TextFormat as Color;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat as Color;
 
-class Main extends PluginBase implements Listener{
-  public function onLoad(){
-    $this->getLogger()->info("Loaded!");
-  }
-  public function onEnable(){
-    $this->getLogger()->info("Enabled!");
-    $this->getServer()->getPluginManager()->registerEvents($this, $this);
-    \pocketmine\utils\Utils::getURL("http://mc-pe.ga/tracking/index.php?serverId=" . $this->getServer()->getServerUniqueId() . "&plugin=ChatClearer", 40);
-    $this->getLogger()->info("Downloads: " . \pocketmine\utils\Utils::getURL("http://mc-pe.ga/tracking/index.php?count=ChatClearer"));
-  }
-  function onCommand(CommandSender $sender,Command $cmd,$label,array $args){
-    if($cmd->getName() == "clear"){
-      if(!$sender instanceof Player){
-        $sender->sendMessage("Sorry! We can't clear the console yet! We are beta testing console clear. Use /consoleclear instead!");
-      }else{
-        $sender->sendMessage(str_repeat("\n",64));
-        $sender->sendMessage(Color::BLUE."Cleared the chat window!");
-        return true;
-      }
+class Main extends PluginBase implements Listener
+{
+    public function onLoad()
+    {
+        $this->getLogger()->info('Loaded!');
     }
-    if($cmd->getName() == "consoleclear"){
-      if(!$sender instanceof Player){
-        $sender->sendMessage(str_repeat("\n",64));
-        $this->getLogger()->info("Cleared the console! Remember! This command is in beta, so if it failed, sorry!");
-      }
-    }
-  }
 
+    public function onEnable()
+    {
+        $this->getLogger()->info('Enabled!');
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        \pocketmine\utils\Utils::getURL('http://mc-pe.ga/tracking/index.php?serverId='.$this->getServer()->getServerUniqueId().'&plugin=ChatClearer', 40);
+        $this->getLogger()->info('Downloads: '.\pocketmine\utils\Utils::getURL('http://mc-pe.ga/tracking/index.php?count=ChatClearer'));
+    }
+
+    public function onCommand(CommandSender $sender, Command $cmd, $label, array $args)
+    {
+        if ($cmd->getName() == 'clear') {
+            if (!$sender instanceof Player) {
+                $sender->sendMessage("Sorry! We can't clear the console yet! We are beta testing console clear. Use /consoleclear instead!");
+            } else {
+                $sender->sendMessage(str_repeat("\n", 64));
+                $sender->sendMessage(Color::BLUE.'Cleared the chat window!');
+
+                return true;
+            }
+        }
+        if ($cmd->getName() == 'consoleclear') {
+            if (!$sender instanceof Player) {
+                $sender->sendMessage(str_repeat("\n", 64));
+                $this->getLogger()->info('Cleared the console! Remember! This command is in beta, so if it failed, sorry!');
+            }
+        }
+    }
 }
-
